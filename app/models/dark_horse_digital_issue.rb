2c78341d149cb8525a6661b2b46b3ae82ec64197
Charles Painter <cpainter85@gmail.com>
2015-04-08 17:27:14 -0600

    removed practice work from dhd model

--- app/models/dark_horse_digital_issue.rb
+++ app/models/dark_horse_digital_issue.rb
@@ -53,21 +53,3 @@ class DarkHorseDigitalIssue < ActiveRecord::Base
   end
 
 end
-
-
-# require 'nokogiri'
-# require 'open-uri'
-#
-# title = ''
-# price = ''
-# publisher = ''
-# doc = Nokogiri::HTML(open('https://digital.darkhorse.com/profile/1773.conan-the-barbarian-1/'))
-#
-# title = doc.css('#profile-title')
-# puts title
-# price = doc.css('a.ajax-submit.button.blue')
-# puts price
-# meta = doc.xpath('//div[contains(@id, profile-meta)]//dd//a[@href]')
-# puts meta.to_s
-# dh_is_publisher = meta.to_s.include? 'Dark Horse Comics'
-# puts dh_is_publisher
