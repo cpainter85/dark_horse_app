f0611b73ef75746ccb9fb86c84118b8c8b85b989
Charles Painter <cpainter85@gmail.com>
2015-04-08 23:00:20 -0600

    re-styled front welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,9 @@
 class WelcomeController < ApplicationController
   before_action :disable_nav
-  
-  def index
 
+  def index
+    @issues = Issue.all.count
+    @volumes = Volume.all.count
+    @creators = Creator.all.count
   end
 end
